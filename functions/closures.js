a16b0063bf493747b7111b373da3dd4fb8496d25
Raphael <raphvillejo@gmail.com>
2020-03-09 21:25:13 +0800

    promise chaining

--- functions/closures.js
+++ functions/closures.js
@@ -1,11 +1,45 @@
-const createTipper = baseTip => {
-  return amount => {
-    return amount * baseTip;
-  };
-};
+const createCounter = () => {
+    let count = 0
 
-const tip1 = createTipper(0.2);
-const tip3 = createTipper(0.3);
+    return {
+        increment() {
+            count++
+        },
+        decrement() {
+            count--
+        },
+        get() {
+            return count
+        }
+    }
+}
 
-console.log(tip1(100));
-console.log(tip3(200));
+const counter = createCounter()
+counter.increment()
+counter.decrement()
+counter.decrement()
+console.log(counter.get())
+
+// Adder
+const add = (a, b) => a + b
+const createAdder = (a) => {
+    return (b) => {
+        return a + b
+    }
+}
+const add10 = createAdder(10)
+console.log(add10(-2))
+console.log(add10(20))
+const add100 = createAdder(100)
+console.log(add100(-90))
+
+// Tipper
+const createTipper = (baseTip) => {
+    return (amount) => {
+        return baseTip * amount
+    }
+}
+const tip20 = createTipper(.2)
+const tip30 = createTipper(.3)
+console.log(tip20(100))
+console.log(tip30(100))
--- oop/person.js
+++ oop/person.js
@@ -1,60 +1,59 @@
 // Prototypal Inheritance
+// myPerson --> Person.prototype --> Object.prototype --> null
 
 class Person {
-  constructor(firstName, lastName, age, likes = []) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.likes = likes;
-  }
-  getBio() {
-    let bio = `${this.firstName} is ${this.age}.`;
+    constructor(firstName, lastName, age, likes = []) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
+    }
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}.`
 
-    this.likes.forEach(like => {
-      bio += ` ${this.firstName} likes ${like}.`;
-    });
+        this.likes.forEach((like) => {
+            bio += ` ${this.firstName} likes ${like}.`
+        })
 
-    return bio;
-  }
-  setName(fullName) {
-    const names = fullName.split(" ");
-    this.firstName = names[0];
-    this.lastName = names[1];
-  }
+        return bio
+    }
+    set fullName(fullName) {
+        const names = fullName.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[1]
+    }
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+class Employee extends Person {
+    constructor(firstName, lastName, age, position, likes) {
+        super(firstName, lastName, age, likes)
+        this.position = position
+    }
+    getBio() {
+        return `${this.fullName} is a ${this.position}.`
+    }
+    getYearsLeft() {
+        return 65 - this.age
+    }
 }
 
 class Student extends Person {
-  constructor(firstName, grade) {
-    super(firstName);
-    this.grade = grade;
-  }
-
-  getGrade() {
-    const isPassing =
-      this.grade > 70 ? "is passing the class" : "is failing the class";
-    return isPassing;
-  }
-
-  setGrade(grade) {
-    this.grade = grade;
-  }
-
-  updateGrade(grade) {
-    this.grade = this.grade + grade;
-  }
+    constructor(firstName, lastName, age, grade, likes) {
+        super(firstName, lastName, age, likes)
+        this.grade = grade
+    }
+    updateGrade(change) {
+        this.grade += change
+    }
+    getBio() {
+        const status = this.grade >= 70 ? 'passing' : 'failing'
+        return `${this.firstName} is ${status} the class.`
+    }
 }
 
-const student1 = new Student("Raph");
-student1.setGrade(75);
-
-console.log(student1.getGrade());
-
-student1.updateGrade(-30);
-console.log(student1.getGrade());
-
-// const me = new Person("Andrew", "Mead", 27, ["Teaching", "Biking"]);
-// me.setName("Alexis Turner");
-// console.log(me.getBio());
-
-// const person2 = new Person("Clancey", "Turner", 51);
-// console.log(person2.getBio());
+const me = new Employee('Andrew', 'Mead', 27, 'Teacher', [])
+me.fullName = 'Clancey Turner'
+console.log(me.getBio())
